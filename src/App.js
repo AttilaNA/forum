aa47338c3e4dfd73e8273b0ad07fd2e42892614b
Attila Nagy <nagyattila86.hu@gmail.com>
2024-03-20 13:58:23 +0100

    Toggle questions of related category

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import Navbar from './components/Navbar';
 import NavbarFixed from './components/NavbarFixed';
 import Categorie from './components/Categorie';
 import dataJson from './data.json';
+import _ from 'lodash'
 import { useState } from 'react'
 
 function App() {
-  const [data] = useState(dataJson);
+  const [data, setData] = useState(dataJson);
+
+  const toggleQuestions = (id) => {
+    const deepCopyOfData = _.cloneDeep(data);
+    switch (id) {
+      case 1:
+        deepCopyOfData.family.questionsAreShown= !deepCopyOfData.family.questionsAreShown;
+        break;
+      case 2:
+        deepCopyOfData.freeTimeAndActivity.questionsAreShown= !deepCopyOfData.freeTimeAndActivity.questionsAreShown;
+        break;
+      case 3:
+        deepCopyOfData.kindergarten.questionsAreShown= !deepCopyOfData.kindergarten.questionsAreShown;
+        break;
+      case 4:
+        deepCopyOfData.school.questionsAreShown= !deepCopyOfData.school.questionsAreShown;
+        break;
+      default:
+        break;
+    }
+    setData(deepCopyOfData);
+  }
 
   return (
     <>
       <Navbar />
       <NavbarFixed />
       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
-        <Categorie data={data}/>
+        <Categorie data={data} toggleQuestions={toggleQuestions}/>
         <div>test</div>
       </div>
     </>
--- src/components/Categorie.jsx
+++ src/components/Categorie.jsx
@@ -1,7 +1,8 @@
 import linq from 'linq';
 import Question from './Question';
 
-const Categorie = ({ data }) => {
+const Categorie = ({ data, toggleQuestions }) => {
+
   return (
     <div>
         <h1>Categories</h1>
@@ -9,8 +10,8 @@ const Categorie = ({ data }) => {
             linq.from(data).select(x => x.value).toArray().map(x => {
                 return (
                     <div key={`Category ${x.id}`}>
-                        <h5 key={`Category ${x.id}`}>{x.category}</h5>
-                        <Question questionsOfCategory={x.questions}/>
+                        <h5 key={`Category ${x.id}`} onClick={() => toggleQuestions(x.id)}>{x.category}</h5>
+                        { x.questionsAreShown && <Question questionsOfCategory={x.questions}/> }
                     </div>
                 )
             })
